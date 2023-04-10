c3f9b7fa0c0c78ada90ffe78f7fed693b086b935
josh <jpollock412@gmail.com>
2023-04-10 10:30:27 -0400

    Hi roy commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import { OpenAI } from "langchain/llms";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+if( ! process.env.OPENAI_API_KEY ){
+    throw new Error("Missing OPENAI_API_KEY environment variable");
+}
+
+export const run = async () => {
+    const model = new OpenAI({ temperature: 0.9,modelName:"text-ada-001" });
+    const res = await model.call(
+        "What would be a good company name for a cat that can fly?"
+    );
+    console.log({ res });
+};
+
+run();
